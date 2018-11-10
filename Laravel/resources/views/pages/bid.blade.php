0c47c679d71bb52520200506f28c86a5aeec4cc9
Ana Santos <anaezes@gmail.com>
2018-11-10 19:51:00 +0000

    Creation of the bid page.

---
+++ Laravel/resources/views/pages/bid.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('title', 'bid')
+
+@section('content')
+
+<!-- proposal Content -->
+<main  data-id="{{$bid}}">
+    <p> in construction ...</p>
+</main>
+
+@endsection
