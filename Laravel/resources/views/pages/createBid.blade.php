97e7d1ce9bb6844d9f6e1dceadb4ad6e23b54bd5
Ana Santos <anaezes@gmail.com>
2018-11-20 22:10:38 +0000

    Modify pages.blade in order to show the teams

--- Laravel/resources/views/pages/createBid.blade.php
+++ Laravel/resources/views/pages/createBid.blade.php
@@ -11,12 +11,12 @@
     </div>
     <hr id="hr_space" class="mt-2">
     <main>
-        <form class="ml-4 mr-4" method="POST" action="{{ route ('createBid', ['id' => $id]) }}" enctype="multipart/form-data">
+        <form class="ml-4 mr-115" method="POST" action="{{ route ('createBid', ['id' => $data[1] ]) }}" enctype="multipart/form-data">
             {{ csrf_field() }}
 
             <div class="form-row">
-                <div class="form-group col-md-12">
-                    <label for="descriptionBid">Description</label>
+                <div class="form-group col-md-10">
+                    <label for="descriptionBid"><i class="fa fa-align-justify"></i> Description</label>
                     <textarea id="descriptionBid" name="descriptionBid" rows="10" cols="30" class="form-control" placeholder="Describe your bid proposal: How will you solve our problem?" value="{{ old('description') }}" required></textarea>
                     @if ($errors->has('descriptionBid'))
                     <span class="error">
@@ -30,34 +30,34 @@
                 <div class="form-group col-md-3"> <!-- todo: get database skills -->
                     <label for="team"><i class="fa fa-calendar"></i> Teams</label>
                     <select name="team" class="form-control" value="{{ old('team') }}" required>
-                        <option value="1">team 1</option>
-                        <option value="2">team 2</option>
-                        <option value="3">team 3</option>
-                        <option value="4">team 4</option>
-                    </select>
-                    @if ($errors->has('team'))
-                    <span class="error">
-                        {{ $errors->first('team') }}
-                      </span>
+                    @if (Auth::check())
+                        @foreach ($data[0] as $team)
+                        <option value="{{$team->id}}" >{{$team->teamname}}</option>
+                        @endforeach
+                        @if(sizeof($data[0]) == 0)
+                        <option value="0" > "Please create a team first..."</option>
+                        @endif
                     @endif
+                    </select>
                 </div>
+            </div>
 
-                <div class="form-group col-md-2">
-                </div>
-
-                <div class="form-group col-md-6"> <!-- todo triggers to database-->
+            <div class="form-row">
+                <div class="form-group col-md-6">
                     <label><i class="fa fa-calendar"></i> Date of submission</label>
-                    <div class="form-row">
-                        <div class="form-group col-md-4">
+
                     <input type="date" name="submissionDate" required>
-                        </div>
                     </div>
                 </div>
 
 
                 <div class="form-group col-md-12">
                     <p><br><br>
-                    <button type="submit" class="btn btn-primary col-md-12">Create Bid</button>
+                        @if(sizeof($data[0]) == 0)
+                        <button id="unbiddable" type="submit" disabled class="btn btn-outline-secondary col-md-6">Unable to bid, create a team first...</button>
+                        @else
+                        <button type="submit" class="btn btn-primary col-md-12">Create Bid</button>
+                        @endif
                     </p>
                 </div>
             </div>
