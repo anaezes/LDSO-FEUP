dd8215d3d9d959b37dba2386061de15b9c713d7e
Ana Santos <anaezes@gmail.com>
2018-11-21 13:44:47 +0000

    Automate logic to get the days and hours

--- Laravel/resources/views/pages/create.blade.php
+++ Laravel/resources/views/pages/create.blade.php
@@ -85,14 +85,10 @@
                         <div class="form-group col-md-2">
                             <label for="days">Days</label>
                             <select id="days" name="days" class="form-control" required>
-                                <option value="">&nbsp;</option>
-                                <option>0</option>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                                <option>6</option>
+                                <option value="0">&nbsp;</option>
+                                @for($i = 0; $i < 14; $i++)
+                                <option value="{{$i}}">{{$i}}</option>
+                                @endfor
                             </select>
                             @if ($errors->has('images'))
                             <span class="error">
@@ -103,32 +99,10 @@
                         <div class="form-group col-md-2">
                             <label for="hours">Hours</label>
                             <select id="hours" name="hours" class="form-control" required>
-                                <option value="">&nbsp;</option>
-                                <option>0</option>
-                                <option>1</option>
-                                <option>2</option>
-                                <option>3</option>
-                                <option>4</option>
-                                <option>5</option>
-                                <option>6</option>
-                                <option>7</option>
-                                <option>8</option>
-                                <option>9</option>
-                                <option>10</option>
-                                <option>11</option>
-                                <option>12</option>
-                                <option>13</option>
-                                <option>14</option>
-                                <option>15</option>
-                                <option>16</option>
-                                <option>17</option>
-                                <option>18</option>
-                                <option>19</option>
-                                <option>20</option>
-                                <option>21</option>
-                                <option>22</option>
-                                <option>23</option>
-                                <option>24</option>
+                                <option value="0">&nbsp;</option>
+                                @for($i = 0; $i < 24; $i++)
+                                <option value="{{$i}}">{{$i}}</option>
+                                @endfor
                             </select>
                             @if ($errors->has('hours'))
                             <span class="error">
@@ -182,8 +156,6 @@
                     </div>
                 </div>
             </div>
-
-
                 <div class="form-group col-md-12">
                     <button type="submit" class="btn btn-primary col-md-12">Create proposal</button>
                 </div>
