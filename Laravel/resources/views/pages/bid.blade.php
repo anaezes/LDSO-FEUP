2f0e081023ab7d40c752ee704a573da8494a081e
José Azevedo <zemafaz@gmail.com>
2018-11-28 19:07:52 +0000

    Fixed layout in submission page

--- Laravel/resources/views/pages/bid.blade.php
+++ Laravel/resources/views/pages/bid.blade.php
@@ -95,12 +95,14 @@
             </tr>
             </tbody>
         </table>
-        @if ($bid->proposal->proposal_status == "finished" && Auth::id() == $bid->team->user->id)
-        <form class="ml-4 mr-4" method="POST" action="{{ route('proposal.notify', $bid->proposal->id) }}" enctype="multipart/form-data">
+        @if ($bid->winner == true && $bid->proposal->proposal_status == "finished" && Auth::id() == $bid->team->user->id && $bid->selfevaluation == null)
+        <form class="ml-4 mr-4" method="POST" action="{{ route('proposal.notify', $bid->proposal->id) }}" enctype="multipart/form-data" style = "margin-top: 5em">
             {{ csrf_field() }}
             <div class="row">
+                <div class="form-group col-md-2" style="padding: 0">
+                    <label for="self_evaluation" style="font-weight: bolder">Self-Evaluation</label>
+                </div>
                 <div class="form-group col-md-3">
-                    <label for="self_evaluation">Self-Evaluation</label>
                     <input id="self_evaluation" class="form-control" type="number" name="self_evaluation" min="1" max="20" required value="1">
                     @if ($errors->has('self_evaluation'))
                         <span class="error">
@@ -108,7 +110,6 @@
                     {{ $errors->first('self_evaluation') }}
                     @endif
                 </div>
-                <br>
                 <div class="form-group col-md-3">
                     <button type="submit" class="btn btn-primary col-md-12">Submit Project</button>
                 </div>
