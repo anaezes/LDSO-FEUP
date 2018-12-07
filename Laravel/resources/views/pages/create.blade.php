9c70bed42513b40727519922815aa9248253c4d0
Arthur26041996 <arthurmatta@hotmail.com>
2018-12-07 17:07:10 +0000

    initial implementation of new page layout

--- Laravel/resources/views/pages/create.blade.php
+++ Laravel/resources/views/pages/create.blade.php
@@ -3,11 +3,178 @@
 @section('title', 'Create proposal')
 
 @section('content')
-    <!-- Content create proposal -->
+<!-- Content create proposal -->
+<div class="container mt-5 mb-5">
+    <div class="row">
+        <div class="col-lg-12">
+            <h4>
+                <i class="fa fa-plus"></i>
+                Create a proposal
+            </h4>
+        </div>
+    </div>
+
+    <hr id="hr_space" class="mt-2">
+
+    <form action="{{ route('create') }}" method="POST" enctype="multipart/form-data">
+        <div class="form-row">
+            <div class="form-group col-lg-12">
+                <h4>
+                    <label for="title">Title</label>
+                </h4>
+                <input type="text" name="title" class="form-control" minlength="10" maxlenght="80" placeholder="Proposal's title" required>
+            </div>
+        </div>
+
+        <hr id="hr_space" class="mt-2">
+
+        <div class="form-row">
+            <div class="form-group col-lg-12">
+                <h4>
+                    <label for="description">Description</label>
+                </h4>
+                <textarea name="description" class="form-control" cols="80" rows="5" minlength="20" placeholder="Proposal's description" required></textarea>
+            </div>
+        </div>
+
+        <hr id="hr_space" class="mt-2">
+
+        <div class="form-row">
+            <div class="form-group col-lg-12">
+                <h4>
+                    <label for="skills">Skills</label>
+                </h4>
+                <select name="skills" class="form-control" size="8"  multiple>
+                    @foreach(App\Skill::orderBy('skillname')->get() as $skill)
+                        <option value="{{ $skill->id }}" class="p-1">{{ $skill->skillname }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <hr id="hr_space" class="mt-2">
+
+        <div class="form-row">
+            <div class="form-group col-lg-12">
+                <h4>
+                    <label for="faculties">Faculties</label>
+                </h4>
+                <select name="faculties" class="form-control" size="8" multiple>
+                    @foreach(App\Faculty::orderBy('facultyname')->get() as $faculty)
+                        <option value="{{ $faculty->id }}" class="p-1">{{ $faculty->facultyname }}</option>
+                    @endforeach
+                </select>
+            </div>
+        </div>
+
+        <hr id="hr_space" class="mt-2">
+
+        <div class="form-row">
+            <div class="form-group col-lg-6">
+                <h4>
+                    <label>Duration</label>
+                </h4>
+                <small style="font-size: small">
+                    maximum of 13 days, 23 hours, and 59 minutes.
+                </small>
+                <div class="form-row mt-3">
+                    <div class="form-group col-lg-4">
+                        <h5>
+                            <label for="days">Days</label>
+                        </h5>
+                        <input type="number" name="days" class="form-control" min="0" max="13" value="0">
+                    </div>
+                    <div class="form-group col-lg-4">
+                        <h5>
+                            <label for="hours">Hours</label>
+                        </h5>
+                        <input type="number" name="hours" class="form-control" min="0" max="23" value="0">
+                    </div>
+                    <div class="form-group col-lg-4">
+                        <h5>
+                            <label for="minutes">Minutes</label>
+                        </h5>
+                        <input type="number" name="minutes" class="form-control" min="0" max="59" value="0">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr id="hr_space" class="mt-2">
+
+        <div class="form-row">
+            <div class="form-group col-lg-8">
+                <h4>
+                    <label>Dates</label>
+                </h4>
+                <div class="form-row mt-3">
+                    <div class="form-group col-lg-5">
+                        <h5>
+                            <label for="annoucement">Winner annoucement date</label>
+                        </h5>
+                        <small>
+                            Date by which a winner must be selected.
+                        </small>
+                        <input type="date" name="annoucement" class="form-control mt-3">
+                    </div>
+                    <div class="col-lg-2"></div>
+                    <div class="form-group col-lg-5">
+                        <h5>
+                            <label for="due">Proposal due date</label>
+                        </h5>
+                        <small>
+                            Date by which a team must submit its project.
+                        </small>
+                        <input type="date" name="due" class="form-control mt-3">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <hr id="hr_space" class="mt-2">
+
+        <div class="form-row">
+            <div class="form-group col-lg-8">
+                <h4>
+                    <label>Privacy</label>
+                </h4>
+                <div class="form-row mt-3">
+                    <div class="form-group col-lg-8">
+                        <h5>
+                            <input type="checkbox" name="public_prop"> Public proposal<br>
+                        </h5>
+                        <small>
+                            The proposal will be visible for every user.
+                        </small>
+                    </div>
+                </div>
+                <div class="form-row mt-3">
+                    <div class="form-group col-lg-8">
+                        <div class="form-check">
+                            <h5>
+                                <input type="checkbox" name="public_bid" class="form-check-input">
+                                <label for="public_bid" class="form-check-label">Public bid</label>
+                            </h5>
+                        </div>
+                        <small>
+                            The list of bids in the proposal page will be visible for every user.
+                        </small>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+
+
+
+
     <div class="container-fluid bg-white">
     <div class="bg-white mb-0 mt-4 pt-4 panel">
         <h4>
-            <i class="fa fa-plus"></i> Create a Proposal</h4>
+            <i class="fa fa-plus"></i>
+            Create a Proposal
+        </h4>
     </div>
     <hr id="hr_space" class="mt-2">
     <main>
