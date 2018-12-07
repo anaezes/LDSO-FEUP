5cbfd589c98669e163d4e661b5ed386e60041507
Arthur26041996 <arthurmatta@hotmail.com>
2018-12-07 00:07:51 +0000

    FIxed cards links no occupying whole card

--- Laravel/resources/views/partials/users.blade.php
+++ Laravel/resources/views/partials/users.blade.php
@@ -1,6 +1,6 @@
 <div class="col-lg-2 mb-3">
-    <div class="card" style="width: 18rem; height: 11rem;">
-        <a href="{{ route('profile', $users[$i]) }}" style="text-decoration: none;">
+    <a href="{{ route('profile', $users[$i]) }}" style="text-decoration: none;">
+        <div class="card" style="width: 17rem; height: 11rem;">
             <div class="card-header">
                 <span>@</span>{{ $users[$i]->username }}
             </div>
@@ -8,6 +8,6 @@
                 <h5 class="card-title">{{ $users[$i]->name }}</h5>
                 <h6 class="card-subtitle mb-2 text-muted">{{ $users[$i]->faculty->facultyname }}</h6>
             </div>
-        </a>
-    </div>
+        </div>
+    </a>
 </div>
