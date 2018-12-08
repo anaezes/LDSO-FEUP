8683924f3b6cd9c1a693f2259efc30aa6de58d4f
pmsf <pedroferreira81@gmail.com>
2018-12-08 22:03:14 +0000

    Added Login,Logout and AuctionsPage acception tests.

--- Laravel/tests/acceptance/acceptanceTest.php
+++ Laravel/tests/acceptance/acceptanceTest.php
@@ -9,11 +9,11 @@ class AcceptanceTests extends TestCase
 
 
     /**
-     * A basic test example.
-     *
+     * Test Homepage.
+     * User-Homepage
      * @return void
      */
-    public function testBasicExample()
+    public function testHomepage()
     {
         $this->visit('/home')
             ->see('Welcome to U.OPENLAB!')
@@ -22,7 +22,7 @@ class AcceptanceTests extends TestCase
 
     /**
      * Test for register user.
-     *
+     *  Visitor-Register
      * @return void
 
      */
@@ -40,26 +40,95 @@ class AcceptanceTests extends TestCase
             ->seePageIs('/faq');
     }
 
+
     /**
-     * Test for logout.
+     * Test for access profile page.
+     *  Member-Profile page
+     * @return void
+
+         Not working!
+    public function testAccessProfile()
+    {
+
+        $this->visit('/home')
+            ->visit('Login')
+            ->type('teste', 'username')
+            ->type('123456', 'password')
+            ->press('LOGIN')
+            ->visit('Profile')
+            ->seePageIs('/profile/1');
+
+    }
+     */
+
+    /**
+     * Test for edit profile page.
      *
      * @return void
 
 
+    public function testEditProfile()
+    {
+        $this->visit('/profile/1')
+            ->press('Edit Info')
+            ->type('Teste1', 'name')
+            ->type('teste1@fe.up.pt', 'email')
+            ->press('Save any new changes')
+            ->seePageIs('');
+
+
+    }
+     */
+    /**
+     * Test for logout.
+     *  Member-Logout
+     * @return void
+     */
 
     public function testLogoutUser()
     {
         $this->visit('/home')
-            ->type('Teste', 'name')
-            ->type('teste', 'username')
-            ->type('teste@fe.up.pt', 'email')
-            ->select('2','idfaculty')
-            ->type('919191919', 'phone')
-            ->type('123456', 'password')
-            ->type('123456', 'password_confirmation')
-            ->press('REGISTER')
-            ->seePageIs('/faq');
+            ->visit('Logout')
+            ->seePageIs('/home');
+
+
     }
+
+
+    /**
+     * Test for login user.
+     *  Visitor-Authentication
+     * @return void
      */
+
+    public function testLoginUser()
+    {
+        $this->visit('/home')
+            ->visit('Login')
+            ->type('teste', 'username')
+            ->type('123456', 'password')
+            ->press('LOGIN')
+            ->seePageIs('/home');
+
+
+    }
+
+    /**
+     * Test for Auctions page
+     * User-Auctions page
+     * @return void
+     */
+
+    public function testAuctionsPage()
+    {
+        $this->visit('/home')
+            ->visit('allproposals')
+            ->seePageIs('/allproposals');
+
+
+    }
+
+
+
 
 }
