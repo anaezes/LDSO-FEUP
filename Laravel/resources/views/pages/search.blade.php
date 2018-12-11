760a5e4ac2924b6818b96b4a55f412a07c64d646
Arthur26041996 <arthurmatta@hotmail.com>
2018-12-11 14:33:51 +0000

    implemented users search result

--- Laravel/resources/views/pages/search.blade.php
+++ Laravel/resources/views/pages/search.blade.php
@@ -16,7 +16,7 @@
         </div>
         <ul class="nav nav-pills mb-3 row" id="pills-tab" role="tablist">
             <li class="nav-item col-lg-4 text-center">
-                <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="#pills-home" role="tab" aria-controls="pills-home" aria-selected="true">Users</a>
+                <a class="nav-link active" id="pills-users-tab" data-toggle="pill" href="#pills-users" role="tab" aria-controls="pills-users" aria-selected="true">Users</a>
             </li>
             <li class="nav-item col-lg-4 text-center">
                 <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="#pills-profile" role="tab" aria-controls="pills-profile" aria-selected="false">Proposals</a>
@@ -26,26 +26,39 @@
             </li>
         </ul>
         <div class="tab-content" id="pills-tabContent">
-            <div class="tab-pane fade show active row" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
-                @forelse ($users as $user)
-                    <div class="col-lg-2 mb-3">
-                        <a href="{{ route('profile', $user) }}" style="text-decoration: none;">
-                            <div class="card" style="width: 17rem; height: 11rem;">
-                                <div class="card-header">
-                                    <span>@</span>{{ $user->username }}
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $user->name }}</h5>
-                                    <h6 class="card-subtitle mb-2 text-muted">{{ $user->faculty->facultyname }}</h6>
-                                </div>
+            @php
+                $i = 0;
+            @endphp
+            <div class="tab-pane fade show active" id="pills-users" role="tabpanel" aria-labelledby="pills-users-tab">
+                <div class="row">
+                    @forelse ($users as $user)
+                        @if ($i != 0 && !($i % 4))
                             </div>
-                        </a>
-                    </div>
-                @empty
-                    <h5 class="text-center text-muted mt-5">
-                        No results found.
-                    </h5>
-                @endforelse
+                            <div class="row">
+                        @else
+                            @php
+                                $i++;
+                            @endphp
+                        @endif
+                        <div class="col-lg-3 mb-3">
+                            <a href="{{ route('profile', $user) }}" style="text-decoration: none;">
+                                <div class="card" style="width: 17rem; height: 11rem;">
+                                    <div class="card-header">
+                                        <span>@</span>{{ $user->username }}
+                                    </div>
+                                    <div class="card-body">
+                                        <h5 class="card-title">{{ $user->name }}</h5>
+                                        <h6 class="card-subtitle mb-2 text-muted">{{ $user->faculty->facultyname }}</h6>
+                                    </div>
+                                </div>
+                            </a>
+                        </div>
+                    @empty
+                        <h5 class="text-center text-muted mt-5">
+                            No results found.
+                        </h5>
+                    @endforelse
+                </div>
             </div>
             <div class="tab-pane fade" id="pills-profile" role="tabpanel" aria-labelledby="pills-profile-tab">...</div>
             <div class="tab-pane fade" id="pills-contact" role="tabpanel" aria-labelledby="pills-contact-tab">...</div>
