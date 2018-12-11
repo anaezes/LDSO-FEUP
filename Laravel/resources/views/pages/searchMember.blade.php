2d3d5cae398d2df3c3cac320fd7dd5e0f82b92a4
Arthur26041996 <arthurmatta@hotmail.com>
2018-12-11 23:36:10 +0000

    updated page to display search member results

--- Laravel/resources/views/pages/searchMember.blade.php
+++ Laravel/resources/views/pages/searchMember.blade.php
@@ -4,64 +4,50 @@
 
 @section('content')
 
-<div class="content p-4">
-    <div class="bg-white mb-0 mt-4 panel">
-        <h4>
-            <i class="fa fa-search-plus"></i> Search results</h4>
+<main>
+    <div class="container mt-5 mb-5">
+        <div class="row">
+            <div class="col-lg-12">
+                <h4>
+                    <i class="fas fa-search"></i>
+                    Search Member
+                </h4>
+                <hr id="hr_space" class="mt-2">
+            </div>
+        </div>
+        @php
+            $i = 0;
+        @endphp
+        <div class="row">
+            @forelse ($users as $user)
+                @if ($i != 0 && !($i % 4))
+                    </div>
+                    <div class="row">
+                @else
+                    @php
+                        $i++;
+                    @endphp
+                @endif
+                <div class="col-lg-3 mb-3">
+                    <a href="{{ route('profile', $user) }}" style="text-decoration: none;">
+                        <div class="card" style="width: 17rem; height: 11rem;">
+                            <div class="card-header">
+                                <span>@</span>{{ $user->username }}
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">{{ $user->name }}</h5>
+                                <h6 class="card-subtitle mb-2 text-muted">{{ $user->faculty->facultyname }}</h6>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            @empty
+                <h5 class="col-lg-12 text-center text-muted mt-5">
+                    No results found.
+                </h5>
+            @endforelse
+        </div>
     </div>
-    <hr id="hr_space" class="mt-2">
-    <main>
-        <a class="btn btn-primary col-md-12 mb-4" data-toggle="collapse" href="#searchMem" aria-expanded="false" aria-controls="advSearch">
-        Advanced search options
-        </a>
-        <form class="ml-4 mr-4 collapse" id ="searchMem" method="POST" action="{{ route('searchMember') }}" enctype="multipart/form-data">
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label class="font-weight-bold">Skills</label>
-                    <select id="skill" name="skill[]" class="form-control" multiple>
-                        <option>Skill1</option>
-                        <option>Skill2</option>
-                        <option>Skill3</option>
-                        <option>Skill4</option>
-                    </select>
-                </div>
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-6">
-                    <label class="font-weight-bold">Faculty</label>
-                    <select id="faculty" name="faculty[]" class="form-control" multiple>
-                        <option>Faculty of Architecture</option>
-                        <option>Faculty of Fine Arts</option>
-                        <option>Faculty of Science</option>
-                        <option>Faculty of Sport</option>
-                        <option>Faculty of Law</option>
-                        <option>Faculty of Economics</option>
-                        <option>Faculty of Engineering</option>
-                        <option>Faculty of Pharmacy</option>
-                        <option>Faculty of Arts</option>
-                        <option>Faculty of Medicine</option>
-                        <option>Faculty of Dental Medicine</option>
-                        <option>Faculty of Psychology and Education Science</option>
-                        <option>Abel Salazar Institute of Biomedical Science</option>
-                        <option>Porto Business School</option>
-                    </select>
-                </div>
-
-            </div>
-            <div class="form-row">
-                <div class="form-group col-md-12">
-                    <button type="submit" class="btn btn-primary col-md-12">Search</button>
-                </div>
-            </div>
-        </form>
-
-
-
-        <!--auctions partial-->
-        @include('pages.memberItems', ['members' => $members])
-
-    </main>
-</div>
-</div>
+</main>
 
 @endsection
